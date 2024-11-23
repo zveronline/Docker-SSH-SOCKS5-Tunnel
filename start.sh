7208b33ad5a4862f9fc25bc32ebbf69f3a6de0e8
Артем <mail@zveronline.ru>
2024-11-23 08:07:48 +0000

    ssh-port

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 
-spawn ssh -4 -D 0.0.0.0:1080 -o StrictHostKeyChecking=no $::env(ssh_user)@$::env(ssh_host)
+spawn ssh -4 -D 0.0.0.0:1080 -o StrictHostKeyChecking=no $::env(ssh_user)@$::env(ssh_host) -p $::env(ssh_port)
 expect "*ssword: "
 send $::env(ssh_pass)\n
 interact
